7b0c0a73258b8b5d7ac14ca75faae4f11002ee4e
Brian Appleton <appleton6509@outlook.com>
2019-11-24 18:34:24 -0700

    added typing effect and btn transition

--- js/introText.js
+++ js/introText.js
@@ -1,35 +1,49 @@
 
 /**
  * 
- * @param {string} textID the id of the element to change
- * @param {*} text to be inserted into the element
- * @param {*} milliseconds ms between the typing of each charactor
- * @param {*} mirrorID is a second id which to type to
+ * @param {String} id the id of the element to change
+ * @param {String} text to be inserted into the element
+ * @param {String} milliseconds ms between displaying the next character in the given string
+ * @param {String} mirrorID is a second id which to type to
+ * @param {Function} callback is a method that is called when the function has completed displaying the text parameter
+ * @param {Function} callbackParam1 paramter for callback
+ * * @param {Function} callbackParam2 paramter for callback
+ * @description a string is displayed on the screen one charactor at a time which creates a typing effect
  */
-function textTyping(textID, text, milliseconds) {
-    var counter = 0;
+function textTyping(id, text, milliseconds, callback, callbackParam1,callbackParam2) {
+    var intervalCounter = 0;
+    
     var timer = setInterval(() => {
-        if (counter < text.length) {
-            document.getElementById(textID).innerHTML += text.charAt(counter);
+        if (intervalCounter < text.length) {
+            document.getElementById(id).innerHTML += text.charAt(intervalCounter);
+            intervalCounter++;
         }
         else {
+            if (callback != undefined) {
+                callback(callbackParam1,callbackParam2);
+            }
             clearInterval(timer);
         }
-        counter++;
     }, milliseconds);
 }
-
-function blinkingCursor(id, milliseconds) {
+/**
+ * 
+ * @param {String} the id of the element where the blinking cursor will appear
+ * @param {String} runTime length of time in milliseconds the function lasts
+ */
+function blinkingCursor(id, runTime) {
     var localMilliseconds = 350;
     var millisecondsPassed = 0;
 
     var timer = setInterval(() => {
         millisecondsPassed += localMilliseconds;
-
-        if (millisecondsPassed > milliseconds) {
+        
+        //current interval is the last, stop the timer and set string field to BLANK
+        if (millisecondsPassed > runTime) {
             clearInterval(timer);
             document.getElementById(id).innerHTML = "&nbsp;";
         }
+        //curent interval is NOT the last proceed with alternative between string charactors
         else if (document.getElementById(id).innerHTML != "|") {
             document.getElementById(id).innerHTML = "|";
         }
@@ -38,9 +52,7 @@ function blinkingCursor(id, milliseconds) {
         }
     }, localMilliseconds);
 }
-function startTyping() {
-    textTyping("introText1", "'Live life with no excuses, ", 75);
-    setTimeout(() => { textTyping("introText1", "travel with no regrets.'", 75); }, 3000);
-    document.getElementById("btnDiscover").style.opacity = "1.0";
-    document.getElementById("btnDiscover").style.marginTop = "90px";
-}
+/**
+ * 
+ */
+
