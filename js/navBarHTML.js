9197b17ebc984eaa7335f937282788801d8011ad
bilal-github <57675296+bilal-github@users.noreply.github.com>
2019-12-02 09:08:34 -0700

    Added navbar logo pictures and added to Navbarhtml.js

--- js/navBarHTML.js
+++ js/navBarHTML.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
  // $("header").append("<hr>");
-  var navBarHTML =
+  var oldBarHTML =
     '<header id="header" class="justify-content-center py-2">\
     <a href="index.html">\
       <img class="logo" src="../images/travel_logo.png" alt="Travel Agency Logo"\
@@ -30,5 +30,28 @@ $(document).ready(function() {
         </ul>\
     </div>\
 </nav>';
-  $("section").append(navBarHTML);
+var navBarHTML =
+    '<nav class="navbar navbar-expand-md bg-dark navbar-dark">\
+    <button class="navbar-toggler justify-content-end" type="button" data-toggle="collapse"\
+        data-target="#collapsibleNavbar">\
+        <span class="navbar-toggler-icon"></span>\
+    </button>\
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">\
+        <div class="navbar-nav" width="100%">\
+                <a class="nav-link" href="index.html">\
+                <img src="plane.png">\
+                Home</a>\
+                <a class="nav-link" href="Contact-details.html">\
+                <img src="card.png">\
+                Contact Us</a>\
+                <a class="nav-link" href="Vacation.html">\
+                <img src="book.png">\
+                Book Vacation</a>\
+                <a class="nav-link" href="CustomerRegistration-Copy.html">\
+                <img src="register.png">\
+                Sign Up</a>\
+        </div>\
+    </div>\
+</nav>';
+  $("section").append(oldBarHTML);
 });
