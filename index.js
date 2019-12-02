fb8b3b758d3239f4494f5b4d7487e08c8d512f42
bilal-github <57675296+bilal-github@users.noreply.github.com>
2019-12-02 14:43:51 -0700

    Removed data from AccessDbCustomers

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const mysql = require("mysql");
+
+const bodyParser = require("body-parser");
 
 var con = mysql.createConnection({
     host: "localhost",
-    user: "brian",
-    password: "condor20",
+    user: "Bilal",
     database: "travelexperts"
 });
 
@@ -13,12 +14,16 @@ var con = mysql.createConnection({
 con.connect((err) => {
     if (err) throw err;
 });
+app.use(express.static('./Views', { extensions: ['html'] }));
+app.use(express.static("./"));
 
 app.use(express.static("modules"));
 app.use(express.static("images"));
 app.use(express.static("js"));
 app.use(express.static("css"));
 app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
 
 //serves out the index page 
 app.get('/', function (req, res) {
@@ -36,6 +41,79 @@ app.get('/getVacationPackages', function (req, res) {
         res.send(result);
     });
 });
+// app.get("/CustomerRegistration", (req, res) => {
+//     res.sendFile("/CustomerRegistration");
+// });
 
-app.listen(8000);
-console.log('8000 is the magic port');
+app.listen(8000, err => {
+    if (err) throw err;
+    console.log("Server started on port 8000");
+});
+
+
+// Code author: Bilal 
+var data = [];
+app.post("/post_form", (req, res) => {
+    console.log(req.body);
+    data[0] = req.body.custFirstName;
+    data[1] = req.body.custLastName;
+    data[2] = req.body.custAddress;
+    data[3] = req.body.custCity;
+    data[4] = req.body.custProv;
+    data[5] = req.body.custPostal;
+    data[6] = req.body.custCountry;
+    data[7] = req.body.custHomePhone;
+    data[8] = req.body.custBusPhone;
+    data[9] = req.body.custEmail;
+    data[10] = req.body.UserName;
+    data[11] = req.body.userPassword;
+
+
+
+    var userNameQuery = "SELECT `UserName` FROM customers WHERE `UserName` = '" + req.body.UserName + "'";
+    var sqlInsert =
+        "INSERT INTO `customers`(`CustFirstName`, `CustLastName`, `CustAddress`, `CustCity`, `CustProv`,"
+        + " `CustPostal`, `CustCountry`, `CustHomePhone`, `CustBusPhone`, `CustEmail`, `UserName`,`UserPassword`)"
+        + " VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+
+    conn.query(userNameQuery, data, (err, results, fields) => {
+        if (err) throw console.log(err);
+        console.log(results);
+        if (results.length > 0) {
+            console.log("Username already exists");
+        } else {
+            conn.query(sqlInsert, data, (err, result, fields) => {
+                if (err) throw err;
+                console.log(result);
+                conn.end(err => {
+                    if (err) throw err;
+                });
+                console.log("Account Registered for Username: " + req.body.UserName);
+            });
+        }
+    })
+
+});
+
+// Code author: Bilal
+app.post("/post_loginForm", (req, res) => {
+    console.log(req.body);
+    var UserName = req.body.uNameLogin;
+    var Password = req.body.passwordLogin;
+
+    conn.connect(err => {
+        if (err) throw err;
+        conn.query("SELECT * FROM customers WHERE `UserName` = ? AND `UserPassword` = ?", [UserName, Password], (err, results, fields) => {
+            if (err) {
+                console.log("Error occured in conn.query");
+            } else {
+                if (results.length > 0) {
+                    console.log("login Successful");
+                    res.redirect("index");
+                } else {
+                    console.log("Username not found: please register");
+                }
+            }
+        });
+    });
+})
